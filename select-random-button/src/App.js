6c049962a135a7bb10f5ce89cc0fa9f0ce96d835
yigitcevk <yigit.cevikk@gmail.com>
2022-02-09 13:25:41 +0300

    App.js added

--- select-random-button/src/App.js
+++ select-random-button/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
+import Buttons from './components/Buttons';
 
-function App() {
+
+
+
+const App = () => {
+  
+  const canvas = useRef(null);
+
+  function rand(items) {
+    // "~~" for a closest "int"
+    return items[Math.floor(Math.random()*items.length)];
+  }
+
+  useEffect(() => {
+    if (canvas) {
+      const ctx = canvas.current.getContext("2d");
+      ctx.fillStyle = "black";
+      ctx.fillRect(0, 0, 1024, 512);
+
+      ctx.fillStyle = "red";
+      ctx.fillRect(0,0,64,64);
+      ctx.fill();
+
+      ctx.fillStyle = "blue";
+      ctx.fillRect(128,256,64,64);
+      ctx.fill();
+
+      ctx.fillStyle = "green";
+      ctx.fillRect(512,128,64,64);
+      ctx.fill();
+
+      ctx.fillStyle = "yellow";
+      ctx.fillRect(800,356,64,64);
+      ctx.fill();
+
+      ctx.font = "20px Comic Sans MS";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+
+
+    }
+  }, [canvas]);  
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="canvas-wrap">
+      <canvas id='myCanvas' ref={canvas} width={1024} height={512} />
+      <div id="overlay">
+        <Buttons/>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
